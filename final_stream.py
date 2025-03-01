78957984629b526f3539cdb02bd19c1f272e2092
omonikpaparao <omonikpaparao@gmail.com>
2025-03-01 21:03:13 +0530

    Update final_stream.py

--- final_stream.py
+++ final_stream.py
@@ -250,12 +250,8 @@ elif menu == "Certificate Generation and Sending":
                         for column in column_names:
                             if column in coordinates:
                                 text = row[column]
-                                if(column=="Roll"):
-                                    pradeep=(str(text).upper()).center(longest[column])
-                                else:
-                                    pradeep=(str(text).title()).center(longest[column])
                                 text_list.append({
-                                    "text": pradeep,
+                                    "text": str(text).center(longest[column]),
                                     "x": coordinates[column]["x"],
                                     "y": coordinates[column]["y"],
                                     "font_name": font_name,
